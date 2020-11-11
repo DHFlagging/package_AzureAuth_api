ff1e7851d4267a8654918b602a1ea683fa13f23c
Joel <jbirch88655@gmail.com>
2020-11-11 14:05:07 -0800

    Initial Commit

---
+++ src/AzureAuthServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace jbirch8865\ReactTour;
+use Illuminate\Support\ServiceProvider;
+
+class ReactTourServiceProvider extends ServiceProvider
+{
+    public function boot() : void
+    {
+
+    }
+
+    public function register() : void
+    {
+
+    }
+}
---
+++ src/Http/Middleware/AzureAuth.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace jbirch8865\AzureAuth\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AzureAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $provider = new \TheNetworg\OAuth2\Client\Provider\Azure([
+            'clientId'          => env('azureClientID'),
+            'clientSecret'      => env('azureClientSecret'),
+            'redirectUri'       => env('azureRedirectUri')
+        ]);
+        try
+        {
+            $token = $provider->validateAccessToken($request->header('Authorization'));
+        }catch(\Exception $e)
+        {
+            return response()->json(["message" => "Unauthorized"],401);
+        }
+        return $next($request);
+    }
+}
